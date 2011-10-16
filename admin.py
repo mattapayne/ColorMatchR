39c6f558211e8c37bf39f74c0678c153acf3ce8a
Matt Payne <paynmatt@gmail.com>
2011-10-16 01:45:27 -0700

    Testing new system

--- admin.py
+++ admin.py
@@ -1,7 +1,7 @@
 from google.appengine.ext import webapp
 from google.appengine.ext import db
 from helpers import *
-
+#This is a test
 class AdminPage(webapp.RequestHandler):
     def get(self):
 		signups = db.GqlQuery("SELECT * FROM BetaSignup")
